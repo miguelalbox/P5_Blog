104e5f3c121d754e90ac462a2980511ea05ced38
miguelalbox <mikyfiestas@gmail.com>
2021-11-29 17:10:38 +0100

    [Features] Add user in BO #21

--- app/Controllers/Back.php
+++ app/Controllers/Back.php
@@ -54,14 +54,50 @@ class Back
                 ]);
                 $msg = "l'auteur à bien été enregistré";
             } catch (\Throwable $err) {
-                die(".....".var_dump($err));
                 $error = true;
                 $msg="un problème est apparu lors de l'enregistrement";
             }
         };
-        $this->template = "backoffice/auteurs/ajouter-auteurs";
+        $this->template = "backoffice/auteurs/ajouter-auteur";
         $this->data = [
             'menu' => 'auteurs',
+            "error" => $error,
+            "message" =>$msg
+        ];
+    }
+    private function users()
+    {
+        
+        $this->template = "backoffice/users/users";
+        $this->data = [
+            'menu' => 'utilisateurs',
+        ];
+    }
+    private function user()
+    {
+        $error = false;
+        $msg = "";
+        if ($this->request->method === "POST") {
+            //die(var_dump($this->request));
+            try {
+                //apppeler model
+                $auteur = new Users();
+                $auteur->ajouteUtilisateur([
+                    "first_name" => $this->request->post["first_name"],
+                    "last_name" => $this->request->post["last_name"],
+                    "email" => $this->request->post["email"],
+                    "password" => $this->request->post["password"],
+                    "civility" => $this->request->post["civility"],
+                ]);
+                $msg = "l'auteur à bien été enregistré";
+            } catch (\Throwable $err) {
+                $error = true;
+                $msg="un problème est apparu lors de l'enregistrement";
+            }
+        };
+        $this->template = "backoffice/users/ajouter-user";
+        $this->data = [
+            'menu' => 'utilisateurs',
             "error" => $error,
             "message" =>$msg
         ];
